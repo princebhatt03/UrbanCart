c665db796b0530aed66f6beea67af2d1fe8483b6
Prince Bhatt <sharmaprince89667@gmail.com>
2025-06-14 11:23:19 +0530

    Bugs Fixed, Merge Done

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-<<<<<<< HEAD
-// https://vite.dev/config/
-=======
->>>>>>> 0dc549ee66ee9f6afdcf2c231a3b4706f713ace5
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
